334397d68402b23f268a3d8a0a2d8c6d4e14a8ac
Davinder Chandhok <davinderc@users.noreply.github.com>
2017-04-25 01:20:59 -0300

    feat: Implement directional gradient code

--- adv_line.py
+++ adv_line.py
@@ -1,6 +1,6 @@
 import cv2
 import calib
-import mult_thresh
+import mult_thresh as mt
 import os
 import numpy as np
 
@@ -16,6 +16,15 @@ for fname in test_fnames:
     cv2.imwrite(test_dir + 'undist_' + fname, undist)
 
 # TODO: 3. Color/gradient threshold
+#dir_grad_r = mt.dir_threshold(undist,sobel_kernel=3, thresh=(np.pi/6,np.pi/3))
+#dir_grad_l = mt.dir_threshold(undist,sobel_kernel=3, thresh=(2*np.pi/3,5*np.pi/6))
+dir_grad_r = mt.dir_threshold(undist,sobel_kernel=3, thresh=(0,np.pi/2))
+
+#combined = np.zeros_like(dir_grad_r)
+#combined[(dir_grad_r == 1) | (dir_grad_l == 1)] = 1
+combined = np.copy(dir_grad_r)
+
+cv2.imwrite(test_dir + 'dir_grad' + fname, combined)
 
 # TODO: 4. Perspective transform
 
