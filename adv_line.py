628b773211c0e7d4d2a48de4b87974368c238d65
Davinder Chandhok <davinderc@users.noreply.github.com>
2017-04-25 00:28:11 -0300

    docs: Comment lane detection pipeline script

---
+++ adv_line.py
@@ -0,0 +1,18 @@
+import cv2
+import calib
+
+# TODO: 1. Camera calibration
+mtx, dist = calib()
+
+# TODO: 2. Distortion correction
+test_dir = './test_images/'
+test_fnames = os.listdir(path=test_dir)
+
+
+# TODO: 3. Color/gradient threshold
+
+# TODO: 4. Perspective transform
+
+# TODO: Detect lane lines
+
+# TODO: Determine lane curvature
