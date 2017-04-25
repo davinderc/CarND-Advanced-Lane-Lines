13cabf26247fbfff59df23564e88438498cbc6ae
Davinder Chandhok <davinderc@users.noreply.github.com>
2017-04-25 01:02:13 -0300

    feat: Add distortion correction to adv. lane detection pipeline

--- adv_line.py
+++ adv_line.py
@@ -1,13 +1,19 @@
 import cv2
 import calib
+import mult_thresh
+import os
+import numpy as np
 
 # TODO: 1. Camera calibration
-mtx, dist = calib()
+mtx, dist = calib.calibrate()
 
 # TODO: 2. Distortion correction
 test_dir = './test_images/'
 test_fnames = os.listdir(path=test_dir)
-
+for fname in test_fnames:
+    img = cv2.imread(test_dir + fname)
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+    cv2.imwrite(test_dir + 'undist_' + fname, undist)
 
 # TODO: 3. Color/gradient threshold
 
