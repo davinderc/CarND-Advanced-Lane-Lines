840013a46169ede704944eed0118443c83dc6f60
Davinder Chandhok <davinderc@users.noreply.github.com>
2017-05-01 17:22:36 -0300

    fix: Adjust perspective transform source points

--- warp_img.py
+++ warp_img.py
@@ -1,26 +1,32 @@
 import cv2
+import os
 import numpy as np
 
 
 def warp():
-    img = cv2.imread('./test_images/straight_lines1.jpg')
-    img_size = (img.shape[1], img.shape[0])
+    test_dir = './test_images/'
+    test_fnames = os.listdir(path=test_dir)
 
-    src = np.float32(
-    [[534, 494],
-     [754, 494],
-     [1007, 655],
-     [299, 655]])
+    for fname in test_fnames:
+        img = cv2.imread(test_dir + fname)
+        img_size = (img.shape[1], img.shape[0])
 
-    dst = np.float32(
-    [[486, 284],
-     [812, 284],
-     [812, 655],
-     [486, 655]])
+        src = np.float32(
+        [[612, 440],
+         [667, 440],
+         [1007, 655],
+         [299, 655]])
 
-    M = cv2.getPerspectiveTransform(src,dst)
-    Minv = cv2.getPerspectiveTransform(dst,src)
+        dst = np.float32(
+        [[486, 284],
+         [812, 284],
+         [812, 655],
+         [486, 655]])
 
-    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
-    cv2.imwrite('./test_images/warpedImg1.jpg',warped)
+        M = cv2.getPerspectiveTransform(src,dst)
+        Minv = cv2.getPerspectiveTransform(dst,src)
+
+        warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+        cv2.imwrite(test_dir + 'warped_' + fname,warped)
+        #return warped
 warp()
