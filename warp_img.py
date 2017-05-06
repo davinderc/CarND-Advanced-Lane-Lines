2abc3da049cb654f766bc06237b4582ea8dbbd30
Davinder Chandhok <davinderc@users.noreply.github.com>
2017-05-06 17:49:26 -0300

    fix: Adjust warp source and destination points

--- warp_img.py
+++ warp_img.py
@@ -3,30 +3,32 @@ import os
 import numpy as np
 
 
-def warp():
-    test_dir = './test_images/'
-    test_fnames = os.listdir(path=test_dir)
+def warp(img):
+    #test_dir = './grad_test_images/'
+    #test_fnames = os.listdir(path=test_dir)
 
-    for fname in test_fnames:
-        img = cv2.imread(test_dir + fname)
-        img_size = (img.shape[1], img.shape[0])
+    #for fname in test_fnames:
+    #img = cv2.imread(test_dir + fname)
+    img_size = (img.shape[1], img.shape[0])
 
-        src = np.float32(
-        [[612, 440],
-         [667, 440],
-         [1007, 655],
-         [299, 655]])
+    offset = 450
 
-        dst = np.float32(
-        [[486, 284],
-         [812, 284],
-         [812, 655],
-         [486, 655]])
+    src = np.float32(
+    [[612, 440],
+     [667, 440],
+     [1007, 670],
+     [299, 670]])
 
-        M = cv2.getPerspectiveTransform(src,dst)
-        Minv = cv2.getPerspectiveTransform(dst,src)
+    dst = np.float32(
+    [[offset, 20],
+     [img_size[0] - offset, 20],
+     [img_size[0] - offset, 655],
+     [offset, 655]])
 
-        warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
-        cv2.imwrite(test_dir + 'warped_' + fname,warped)
-        #return warped
-warp()
+    M = cv2.getPerspectiveTransform(src,dst)
+    Minv = cv2.getPerspectiveTransform(dst,src)
+
+    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+    #cv2.imwrite(test_dir + 'warped_' + fname,warped)
+    return warped
+#warp()
